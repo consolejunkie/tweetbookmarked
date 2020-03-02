a08b87c9821795222a348333a8e241ae8fca71f4
Anvesh Checka <geraldchecka@users.noreply.github.com>
2020-03-02 09:00:28 +0530

    temporary changes

---
+++ main.js
@@ -0,0 +1,12 @@
+// Loop through a long list
+// scroll position
+// Intersection observer
+// Implementing DOM recycling
+// Mutation observer
+
+let
+  root = document.getElementById("root"),
+  list = document.getElementById("list");
+
+console.log(root);
+console.log(list);
